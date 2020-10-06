d79e61008551f721130eb74861b4b0ad730bafaa
danielsotopachaqtec <daniel.soto@pachaqtec.edu.pe>
2020-10-05 21:19:11 -0500

    [feature] - data types in javascript examples

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+// Data Types
 console.log('hola');
+var ccc = 5;
+const hello = () => {
+	let bb = 2;
+	return bb;
+};
+ccc = 666;
+let a; //undefined
+let ba = 20; // is a number
+let bb = NaN;
+let c = 'Daniel'; // is a string
+let d = true; // is a boolean
+let e = {
+	name: 'Daniel',
+	lastname: 'Soto',
+	age: 27,
+	isStudent: false,
+	work: ['Avantica', 'Belcorp'],
+}; // is an object
+let f = ['Apple', 'Pear', 'Watermelon'];
+let h = [
+	{
+		name: 'Daniel',
+		lastname: 'Soto',
+		age: 27,
+		isStudent: false,
+		work: ['Avantica', 'Belcorp'],
+	},
+	{
+		name: 'Amelie',
+		lastname: 'Soto',
+		age: 5,
+		isStudent: true,
+		work: [],
+	},
+];
+console.log('shark' + 5 + 5);
